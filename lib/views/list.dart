1dd910a26809c40dbb6e54de71aa96d0c6fa51dc
Esther145 <esther.thomson98@gmail.com>
2020-05-14 01:08:26 +0400

    College List

--- lib/views/list.dart
+++ lib/views/list.dart
@@ -10,7 +10,11 @@ class _MylistState extends State<Mylist> {
   TextEditingController rollno=TextEditingController();
   TextEditingController adno=TextEditingController();
   TextEditingController college=TextEditingController();
-
+ var names=[];
+ var rolls=[];
+ var adnos=[];
+ var coll=[];
+ //int _index=0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,13 +24,17 @@ class _MylistState extends State<Mylist> {
         body:
             
         SingleChildScrollView(
+
           
           child: Container(
+
+
             margin: EdgeInsets.all(20.0),
             child: Column(
-              
+
 
               children: <Widget>[
+
                 SizedBox(height: 20.0,),
                 TextField(
                   
@@ -63,6 +71,56 @@ class _MylistState extends State<Mylist> {
                   border: OutlineInputBorder(),
                 ),),
                 SizedBox(height: 10.0,),
+                RaisedButton(
+                  child: Text("Add Details",style: TextStyle(color: Colors.white),),
+                  color: Colors.green,
+                  onPressed: ()
+                {
+                  var dname=name.text;
+                  var droll=rollno.text;
+                  var dadno=adno.text;
+                  var dcollege=college.text;
+                  //var details=[{"name":dname,"adno":dadno,"college":dcollege,"rollno":droll}];
+
+
+                  setState(() {
+
+                    names.add(dname);
+                    rolls.add(droll);
+                    adnos.add(dadno);
+                    coll.add(dcollege);
+
+                  });
+                },
+
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount:names.length==null?0:names.length ,
+                  itemBuilder: (context,index)
+                {
+                  return Card(
+                    elevation: 19.0,
+                    child: ListTile(
+                      leading: Icon(Icons.account_circle,size: 40.0,color: Colors.green,),
+                    trailing: GestureDetector(
+                        onTap: ()
+                        {
+                          setState(() {
+                            names.removeAt(index);
+                            rolls.removeAt(index);
+                            adnos.removeAt(index);
+                            coll.removeAt(index);
+                          });
+                        },
+                        child: Icon(Icons.clear,size: 40.0,color: Colors.green,)),
+                    title: Text(names[index].toString()),
+                      subtitle: Text("Roll No:"+rolls[index].toString()+"\nAdno:"+adnos[index].toString()+"\nCollege:"+coll[index].toString()),
+
+                    ),
+                  );
+                },
+                ),
               ],
             ),
           ),
